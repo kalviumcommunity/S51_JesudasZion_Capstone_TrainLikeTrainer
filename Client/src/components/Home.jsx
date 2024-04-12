32517da20e742f1b573cf61b0ced38fbb620e1f7
Zion203 <jesudas.t@kalvium.community>
2024-04-12 13:48:06 +0530

    home page design

--- Client/src/components/Home.jsx
+++ Client/src/components/Home.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
 import NavBar from './NavBar'
+import "../CSS_files/Home.css"
+
 function Home() {
   return (
     <>
         <NavBar></NavBar>
-        <div>Home</div>
+        <div id='homeContainer'>
+          <section id='left_home'>
+          <div id='userWelcome'>
+            <p>Hi Jesudas</p>
+            <p>It's nice to see you again.</p>
+
+            </div>
+
+            <div id='dailyScore'>
+
+            </div>
+
+            <div id='home_buttons'>
+              <div id='course'>
+
+              </div>
+              <div id='forum'>
+
+              </div>
+            </div>
+          </section>
+
+          <section id='right_home'>
+                <p>Latest Sports News</p>
+                <div id='newsContainer'>
+                <div className='newsContent'></div>
+                <div className='newsContent'></div>
+                <div className='newsContent'></div>
+                <div className='newsContent'></div>
+                <div className='newsContent'></div>
+                <div className='newsContent'></div>
+                </div>
+          </section>
+        </div>
     </>
   )
 }
